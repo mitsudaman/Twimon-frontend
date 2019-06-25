eb8171fba7ea7bd3395098be4b9332aa8f92490d
mitsudaman <k.m.masato-uver-aquarius@hotmail.co.jp>
2019-06-25 16:43:11 +0900

    Twitterリダイレクト用ページ追加

---
+++ pages/redirect.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>Twitterへリダイレクトしています{{getRedirectUrl}}</p>
+</template>
+
+<script>
+import GET_REDIRECT_URL_GQL from '~/apollo/queries/getRedirectUrl.gql'
+export default {
+  // middleware: 'guest',
+
+  apollo: {
+    getRedirectUrl: {
+      query: GET_REDIRECT_URL_GQL,
+      update(data){
+        window.location.href = data.getRedirectUrl
+      }
+    },
+  },
+}
+</script>

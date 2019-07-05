14a2767d3b1c5c7dc7ab4318a2a3afaed60a606b
mitsudaman <k.m.masato-uver-aquarius@hotmail.co.jp>
2019-07-05 18:26:13 +0900

    ログイン処理をmutation化、認証済みユーザー取得gql追加

--- pages/callback.vue
+++ pages/callback.vue
@@ -9,7 +9,8 @@
 
 <script>
 import { mapMutations } from 'vuex'
-import GET_SOCIAL_AUTHED_USER from '~/apollo/queries/getSocialAuthedUser.gql'
+import AUTHENTICATE_USER_GQL from '~/apollo/mutations/authenticateUserGql.gql'
+import GET_AUTHED_USER from '~/apollo/queries/getAuthedUser.gql'
 
 export default {
   // middleware: 'guest',
@@ -20,11 +21,12 @@ export default {
     }
   },
   created (){
-    this.$apollo.query({
-        query: GET_SOCIAL_AUTHED_USER, 
+    this.$apollo.mutate({
+        mutation: AUTHENTICATE_USER_GQL, 
       })
       .then(({data}) => { 
         // Query work fine and result is the proper data object
+        console.log(data.authenticateUserGql.access_token)
         // this.$router.replace('/');
       })
       .catch((error) => {
@@ -32,6 +34,20 @@ export default {
         // console.log(error);
         this.failedMessage = 'Twitterでのログインに失敗しました。';
       });
+
+    this.$apollo.query({
+        query: GET_AUTHED_USER,
+        variables: {
+            var1: "this is a variable"
+        },
+        context: {
+          headers: {
+              "Authorization":"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjMxMzNhYTliMDhjYjQ5Mzc2MTU3MzI4ODU2ZDhjYjFlZTQ2MjdmZDcxZGM2YzVmZjU4MmIwMGRjMzcyNmEwZTExODk1NjM4YTcyMjQxNTk0In0.eyJhdWQiOiIxIiwianRpIjoiMzEzM2FhOWIwOGNiNDkzNzYxNTczMjg4NTZkOGNiMWVlNDYyN2ZkNzFkYzZjNWZmNTgyYjAwZGMzNzI2YTBlMTE4OTU2MzhhNzIyNDE1OTQiLCJpYXQiOjE1NjIzMTAzMjIsIm5iZiI6MTU2MjMxMDMyMiwiZXhwIjoxNTkzOTMyNzIyLCJzdWIiOiI5Iiwic2NvcGVzIjpbXX0.H8REUy29uvoooHA6ggU86VI7iTYZ3pCKexFqUYo6ZJwCUQZo4MpOrHt4X7XAa-PhcnhsvnbomSpkIAyzrFCpc06kPr5t-LzTHaVi9zO6eFjFr_qWgqI9A2vYu4uYZNXdwiEgA10ODN3WvH6zUvucAROCfAtRNC5KXf8kbvsskkB-8VsXKSoEDxKhJ599AEdkwLDL-naUeB3vN0i6ZN8ukNPXmaB1IK8MY-GXKB-0lCOCdmInr7_qdo_--S2dEu7GMHF1fNEVbP_HvHPYBxnP21VfSjexYNSx9B_IkSWJVNs1w2pUGqCCqF-k9u7k7Bskvfk3OgHUPQuB1YWvk38OC-rYsv3blY07zKWf0jgOr4eGhe87-MEetaYqq2tZwrtLfGeBjgC3uaej8Hcq7H9qkw_1HwskulWwZSowtA5i67hfL-ZHOjYQD14HOMq5AO2Tz8wfMRUApucF9088tW_Jh-qI0cZV8qlG6KRQtBUjur5bwfheTRkOtiFzTpalRmbrQbNKtoLNurxQsZ5pgLRBsdX1NKRRzOIDNKxcwDDLOn6ETCYlquOm6UT68hs81GNBUeZQidLwIDI5eIlXeDr8rqJt04S_nrzSAzOIpraVn7b3pjiSX1zrJ-3ij-eEqMqScdEofS1FvOn6ISPxZiF8A0gBxvMNiAwqRxW3yNxO840"
+          }
+        }
+    }).then(({data}) => {
+        console.log(data)
+      })
   },
   computed: {
     attempting () {

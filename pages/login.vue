16854679004e2833b1cb922a2608359dc9682417
mitsudaman <k.m.masato-uver-aquarius@hotmail.co.jp>
2019-09-29 22:41:52 +0900

    axios iphone 対策10

--- pages/login.vue
+++ pages/login.vue
@@ -67,7 +67,7 @@ export default {
       // }).then(({data}) => {
       //   window.location.href = data.getRedirectUrl
       // })
-      axios.get(process.env.baseUrl + 'api/login/twitter' + '?nocache=' + new Date().getTime(), {
+      axios.get(process.env.baseUrl + 'api/login/twitter/' + '?nocache=' + new Date().getTime(), {
         headers: { 'Cache-Control': 'no-cache', 'Pragma': 'no-cache', 'Expires': 0 },
         withCredentials: true,
         xsrfHeaderName: 'X-CSRF-Token'

7c7db3ef8d876f358e46f2b34d212e305338a7dd
mitsudaman <k.m.masato-uver-aquarius@hotmail.co.jp>
2019-09-30 13:26:43 +0900

    middrlwareでトークンが無効な場合にログインへ飛ばすように変更

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,6 +1,17 @@
-export default function ({app, redirect,error}) {
+import GET_ME_GQL from '~/apollo/queries/getMe.gql'
+
+export default function ({app, store, redirect, error}) {
   const hasToken = !!app.$apolloHelpers.getToken()
   if (!hasToken) {
     return redirect('/login')
   }
+  const client = app.apolloProvider.defaultClient
+  client.query({
+      query: GET_ME_GQL,
+    }).then(({data}) => {
+        const me = data.me
+        store.commit('setUser',  { user: data.me})
+    }).catch(() => {
+      return redirect('/login')
+    })
 }
